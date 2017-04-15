9a0aa0f978321fc10e85f5481d580cbc65fec514
Hack Reactor Student <student@hackreactor.com>
2017-04-14 19:03:48 -0700

    changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,27 +1,35 @@
 angular.module('video-player')
 
+.controller('appControl', function($scope) {
+  $scope.videos = exampleVideoData;
+})
+
+
 .directive('app', function() {
 
   // $scope.videos = exampleVideoData;
   return {
 
     scope: {
-      videos: '=',
-    },
-
-    // controller: function() {
-    //   // this.videos = exampleVideoData;
-    //   this.videos = 'hi';
-    // },
-    controller: function($scope) {
-      $scope.videos = exampleVideoData;
-      $scope.carName = 'Volvo';
-      $scope.currentVideo = exampleVideoData[0]
+      videos: '<',
     },
 
     controllerAs: 'ctrl',
     bindToController: true,
     restrict: 'E',
+    // controller: function() {
+    //   // this.videos = exampleVideoData;
+    //   this.videos = 'hi';
+    // },
+    // controller: function($scope) {
+    //   $scope.videos = exampleVideoData;
+    //   $scope.carName = 'Volvo';
+    //   $scope.currentVideo = exampleVideoData[0]
+    // },
+    controller: function() {
+      // this.test = 'hello';
+    },
+
     templateUrl: 'src/templates/app.html'
 
   };
